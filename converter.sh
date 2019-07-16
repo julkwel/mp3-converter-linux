d8de5672fdad096276ebe9d343466f9259d0ef1b
Jul <julienrajerison5@gmail.com>
2019-07-16 13:11:48 +0300

    add mp4 converter

--- converter.sh
+++ converter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find . -type f -iname "*.webm" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";' _ '{}' \;
 find . -type f -name "*.mkv" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn $
+find . -type f -name "*.mp4" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -b:a 192K -vn "${FILE%.mp4}.mp3";' _ '{}' \;
